4492e177d479038a6bf6610ed854b37dd119aadc
mehrshaddarzi <mehrshad198@gmail.com>
2021-10-02 12:09:01 +0330

    Add `start` Command

--- command.php
+++ command.php
@@ -5,5 +5,9 @@ if (!class_exists('WP_CLI')) {
     return;
 }
 
+# Define Constant
+define("WP_CLI_TRAIT_PATH", dirname(__FILE__));
+define("WP_CLI_TRAIT_TEMPLATE_PATH", WP_CLI_TRAIT_PATH . '/templates/');
+
 # Register 'trait' Command
 WP_CLI::add_command('trait', WP_Trait_Command::class);
--- src/WP_Trait_Command.php
+++ src/WP_Trait_Command.php
@@ -75,13 +75,140 @@ class WP_Trait_Command extends WP_CLI_Command
      * ## EXAMPLES
      *
      *      # Set new config
-     *      $ wp global-config set path ~/wp-cli/site
-     *      Success: Saved path config.
+     *      $ wp trait start
+     *      Success: Created plugin files.
      *
      */
     public function start($_, $assoc)
     {
-        WP_CLI::success("Saved " . WP_CLI_Helper::color("Salam", "Y") . " config.");
+        # Force Prompt
+        $before_command = $this->getCommandLog();
+        if (!isset ($assoc['prompt']) and count($assoc) < 2) {
+            if (empty($before_command) || (isset($before_command['command']) and $before_command['command'] != "start")) {
+                $this->saveLastCommand('start', $_, $assoc);
+                \WP_CLI::runcommand("trait start --prompt");
+                return;
+            }
+        }
+        $this->removeCommandLog();
+
+        # Sanitize Slug
+        if (!isset($assoc['slug'])) {
+            \WP_CLI::error("Invalid plugin slug specified. please try again.");
+        }
+        if (in_array($assoc['slug'], ['.', '..'], true)) {
+            \WP_CLI::error("Invalid plugin slug specified. The slug cannot be '.' or '..'.");
+        }
+        $plugin_slug = $assoc['slug'];
+        $plugin_name = ucwords(str_replace('-', ' ', $plugin_slug));
+        $plugin_package = str_replace(' ', '_', $plugin_name);
+
+        # Sanitize NameSpace
+        if (!isset($assoc['namespace']) || (isset($assoc['namespace']) and empty($assoc['namespace']))) {
+            $plugin_namespace = str_replace(' ', '_', $plugin_name);
+        } else {
+            $plugin_namespace = trim($assoc['namespace']);
+        }
+        $plugin_namespace = str_ireplace("-", "_", $plugin_namespace);
+        $plugin_namespace = str_ireplace(["/", "\\", "."], "", $plugin_namespace);
+
+        # Run Scaffold Command
+        $defaults = [
+            'plugin_name' => $plugin_name,
+            'plugin_description' => 'plugin description',
+            'plugin_author' => 'plugin author name',
+            'plugin_author_uri' => 'your site here',
+            'plugin_uri' => 'plugin site here',
+            'activate-network' => 0,
+            'activate' => 0,
+            'force' => 0,
+            'skip-tests' => 1
+        ];
+        $data = wp_parse_args($assoc, $defaults);
+        $wp_plugin_dir = str_ireplace("\\", "/", WP_PLUGIN_DIR);
+        $plugin_dir = $wp_plugin_dir . "/{$plugin_slug}";
+        $plugin_main_file = $wp_plugin_dir . "/{$plugin_slug}/{$plugin_slug}.php";
+        \WP_CLI::run_command(['scaffold', 'plugin', $plugin_slug],
+            array_diff_key($data, array_flip(['namespace', 'slug'])));
+
+        # Add ComposerJson File
+        $mustache = \WP_CLI_FileSystem::load_mustache(WP_CLI_TRAIT_TEMPLATE_PATH);
+        \WP_CLI_FileSystem::file_put_content(
+            $plugin_dir . '/composer.json',
+            $mustache->render('composer', [
+                'namespace' => (!empty($plugin_namespace) ? $plugin_namespace . '\\\\' : '')
+            ])
+        );
+
+        # Add To Plugin File
+        \WP_CLI_FileSystem::search_replace_file(
+            $plugin_main_file,
+            array(
+                "// Your code starts here."
+            ),
+            array(
+                $mustache->render('plugin', [
+                    'define_namespace' => "",
+                    'plugin_class' => (!empty($plugin_namespace) ? $plugin_namespace : $plugin_package),
+                    'plugin_slug' => trim($assoc['slug']),
+                ])
+            ));
+
+        # Run Composer Update
+        \WP_CLI_Helper::run_composer($plugin_dir, array('update'));
+
+        # Show Success
+        \WP_CLI::success("Created `" . \WP_CLI_Helper::color(trim($assoc['slug']), "Y") . "` plugin files.");
+    }
+
+    protected function getLogFilePath()
+    {
+        return \WP_CLI_Helper::get_cache_dir('trait') . '/log.json';
+    }
+
+    protected function removeCommandLog()
+    {
+        //Command log file name
+        $file = $this->getLogFilePath();
+        if (file_exists($file)) {
+            \WP_CLI_FileSystem::remove_file($file);
+        }
+    }
+
+    protected function saveLastCommand($command, $args, $assoc_args)
+    {
+        //Command log file name
+        $file = $this->getLogFilePath();
+
+        //Get now Command
+        $now = array(
+            'command' => $command,
+            'args' => $args,
+            'assoc_args' => $assoc_args
+        );
+
+        //Add new Command to Log
+        \WP_CLI_FileSystem::create_json_file($file, $now);
+    }
+
+    protected function getCommandLog()
+    {
+        //Command log file name
+        $file = $this->getLogFilePath();
+        if (file_exists($file)) {
+            # Check time age cache [ 2 minute ]
+            if (time() - filemtime($file) >= 120) {
+                $this->removeCommandLog();
+            } else {
+                # get json parse
+                $json = \WP_CLI_FileSystem::read_json_file($file);
+                if ($json != false) {
+                    return $json;
+                }
+            }
+        }
+
+        return array();
     }
 
     /**
